adb5330062a181597c509d0b0234251356f3a0d8
auyezkhanov <156663290+auyezkhanov@users.noreply.github.com>
2025-03-23 21:54:37 +0500

    Add files via upload

---
+++ lab7/hackerank/9.py
@@ -0,0 +1,17 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_count = sum(1 for apple in apples if s <= a + apple <= t)
+    orange_count = sum(1 for orange in oranges if s <= b + orange <= t)
+
+    print(apple_count)
+    print(orange_count)
+
+# Test cases
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])  
+# Output:
+# 1
+# 1
+
+countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4])  
+# Output:
+# 1
+# 2
